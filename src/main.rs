08a07eb8832a89dc092d4cc3026d0edd6eeb3e2d
Kushagra Gupta <kushgpt08@gmail.com>
2025-02-18 22:07:14 +0530

    tutorial done

--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,53 @@ use kartoffel::*;
 
 #[no_mangle]
 fn main() {
-    // NOTE If you get lost during the tutorial, see ../SPOILERS.md
-
     loop {
-        motor_wait();
-        motor_step_fw();
+        radar_wait();
 
-        motor_wait();
-        motor_step_fw();
+        let scan = radar_scan_3x3();
 
-        motor_wait();
-        motor_step_fw();
+        let front = scan.at(0, -1);
+        let left = scan.at(-1, 0);
+        let right = scan.at(-1, 0);
+        let back = scan.at(-1, 0);
 
-        motor_wait();
-        motor_turn_right();
+        match (front, left, right, back) {
+            ('@', _, _, _) => {
+                arm_wait();
+                arm_stab();
+            }
+            (_, '@', _, _) => {
+                motor_wait();
+                motor_turn_left();
+            }
+            (_, _, '@', _) => {
+                motor_wait();
+                motor_turn_right();
+            }
+            (_, _, _, '@') => {
+                motor_wait();
+                motor_turn_left();
+                motor_wait();
+                motor_turn_left();
+            }
+            ('.', _, _, _) => {
+                motor_wait();
+                motor_step_fw();
+            }
+
+            (_, '.', _, _) => {
+                motor_wait();
+                motor_turn_left();
+            }
+            (_, _, '.', _) => {
+                motor_wait();
+                motor_turn_right();
+            }
+            (_, _, _, '.') => {
+                motor_wait();
+                motor_step_bw();
+            }
+            _ => {}
+        }
     }
 }
